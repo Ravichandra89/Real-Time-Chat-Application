7ec4466e1fecbb14298fe3349ee0e6f1aee0b7ac
Ravi chandra <134200599+Ravichandra89@users.noreply.github.com>
2024-12-22 22:35:27 +0530

    Update user.controller.ts

---
+++ Backend/user_service/src/controller/user.controller.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+export const loginUser = async (req: Request, res: Response) => {
+  
+}
+
+export const RegisterUser = async (req: Request, res: Response) => {
+
+}
+
+export const logOutUser = async (req: Request, res: Response) => {
+
+}
+
+
