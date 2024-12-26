dd070aa9f81f363cc5ff8aee2824b197cd40e24d
Ravi chandra <githubravi01@gmail.com>
2024-12-27 01:20:42 +0530

    User Controller + Router

--- Backend/user_service/src/controller/user.controller.ts
+++ Backend/user_service/src/controller/user.controller.ts
@@ -1,15 +1,19 @@
 import express from "express";
+import apiResponse from "../apiResponse";
+import prisma from "../config/prisma.config";
 
 export const loginUser = async (req: Request, res: Response) => {
-  
-}
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return apiResponse(res, 404, false, "Missing Email and Password");
+    };
+  } catch (error) {
+    console.error("Error logging User", error);
+    return apiResponse(res, 500, false, "Error logging User");
+  }
+};
 
-export const RegisterUser = async (req: Request, res: Response) => {
+export const RegisterUser = async (req: Request, res: Response) => {};
 
-}
-
-export const logOutUser = async (req: Request, res: Response) => {
-
-}
-
-
+export const logOutUser = async (req: Request, res: Response) => {};
